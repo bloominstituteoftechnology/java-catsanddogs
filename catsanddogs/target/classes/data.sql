e4984f6c441d692bdb096a76fa9b8f61a3396ee0
John Mitchell <jrmmba@outlook.com>
2020-07-20 08:11:26 -0700

    Updating Documentation

--- catsanddogs/target/classes/data.sql
+++ catsanddogs/target/classes/data.sql
@@ -12,4 +12,10 @@ INSERT INTO DOGS (dogid, dogname)
 INSERT INTO CATS (catid, catname)
         VALUES (1, 'Tiger');
 
+/*
+We must tell hibernate the ids that have already been used.
+The number must be larger than the last used id.
+15 > 3 so we are good!
+ */
+
 alter sequence hibernate_sequence restart with 15;
